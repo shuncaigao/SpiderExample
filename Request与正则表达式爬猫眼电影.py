8dd3638c136d282311d49278c4e1a0fd36017bd1
shuncaigao <shuncaigao@gmail>
2018-01-17 23:36:05 +0800

    添加Request与正则表达式爬猫眼电影

---
+++ Request与正则表达式爬猫眼电影.py
@@ -0,0 +1,46 @@
+import json
+import re
+
+import requests
+import time
+
+
+def get_one_page(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+def parse_one_page(html):
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(.*?)</i>.*?poster-default.*?src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime">(.*?)</p>.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>',
+        re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score': item[5].strip() + item[6].strip()
+        }
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        write_to_json(item)
+
+
+def write_to_json(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        print(type(json.dumps(content)))
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
